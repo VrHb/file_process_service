e13ebcac29558f961762d7995cffcc061f8eeeee
Viktor Abdukarimov <viredmi@gmail.com>
2023-09-30 22:53:00 +0300

    Add serilizer fields from model

--- fileprocessapp/serializers.py
+++ fileprocessapp/serializers.py
@@ -4,28 +4,10 @@ from rest_framework import serializers
 from fileprocessapp.models import File
 
 
-class FileSerializer(serializers.Serializer):
-    file = serializers.FileField()
-    uploaded_at = serializers.DateTimeField(read_only=True)
-    processed = serializers.BooleanField(read_only=True)
-
-    def create(self, validated_data):
-        file = validated_data.get('file')
-        uploaded_at = timezone.now()
-        processed = False
-        return File.objects.create(
-            file=file,
-            uploaded_at=uploaded_at,
-            processed=processed
-        )
-
-    def update(self, instance, validated_data):
-        instance.processed = validated_data.get(
-            'processed',
-            instance.processed
-        )
-        instance.save()
-        return instance
+class FileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = File
+        fields = ('__all__')
 
 
 
--- fileprocessapp/views.py
+++ fileprocessapp/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http import JsonResponse
 from django.utils import timezone
 
@@ -10,24 +12,29 @@ from loguru import logger
 from .serializers import FileSerializer
 from .models import File
 from .tasks import process_file
-from file_process_service.settings import MEDIA_ROOT
-
 
 
 class FileUploadView(APIView):
     parser_classes = (MultiPartParser, )
-    
+
     @logger.catch()
     def post(self, request):
-        serializer = FileSerializer(data=request.data)
+        data = {
+            'file': request.FILES.get('file'),
+            'uploaded_at': timezone.now(),
+            'processed': False
+        }
+        serializer = FileSerializer(data=data)
         if serializer.is_valid():
             file_object = serializer.save()
+            logger.info(file_object)
             file_path = file_object.file.path 
             try:
                 file_processed = process_file.delay(file_path)
             except:
                 logger.debug(
-                    "Redis not run or some problem with celery, file not processed")
+                    "Redis not run or some problem with celery, file not processed"
+                )
                 file_processed = False
             if file_processed:
                 file_object.processed = True
